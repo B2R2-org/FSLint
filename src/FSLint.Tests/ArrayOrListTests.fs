d24e455c4c81f00031e0f158bb893060e2ba297c
Seung Il Jung <sijung@kaist.ac.kr>
2025-06-20 12:49:06 +0900

    [Test] Add FunApp spacing in list/array

--- src/FSLint.Tests/ArrayOrListTests.fs
+++ src/FSLint.Tests/ArrayOrListTests.fs
@@ -136,13 +136,17 @@ let bad = [
   3(* Bad *) ]
 """
 
-  let goodArraySpaceOperatorsTest = """
-[| this.Address + uint64 this.Length + this.Name |]
+  let goodListSpaceInfixTest = """
+[ this.Address + uint64 this.Length + this.Name ]
 """
 
-  let badArraySpaceOperatorsTest = """
-[| this.Address +  uint64 this.Length + this.Name |]
+  let badListSpaceInfixTest = """
+[ this.Address +  uint64 this.Length + this.Name ]
 """
+
+  let goodListSpaceFunAppTest = """[ fn 1 2 3 x ]"""
+
+  let badListSpaceFunAppTest = """[ fn  1 2 3 x ]"""
 
   [<TestMethod>]
   member _.``[ArrayOrList] List Empty Test`` () =
@@ -255,10 +259,17 @@ let bad = [
     ) |> ignore
 
   [<TestMethod>]
-  member _.``[ArrayOrList] Array Space Before and After Operators Test`` () =
-    linterForFs.Lint Constants.FakeFsPath goodArraySpaceOperatorsTest
+  member _.``[ArrayOrList] List Space Before and After Infix Test`` () =
+    linterForFs.Lint Constants.FakeFsPath goodListSpaceInfixTest
     Assert.ThrowsException<LintException> (fun () ->
-      linterForFs.Lint Constants.FakeFsPath badArraySpaceOperatorsTest
+      linterForFs.Lint Constants.FakeFsPath badListSpaceInfixTest
+    ) |> ignore
+
+  [<TestMethod>]
+  member _.``[ArrayOrList] List Space Fununction Application Test`` () =
+    linterForFs.Lint Constants.FakeFsPath goodListSpaceFunAppTest
+    Assert.ThrowsException<LintException> (fun () ->
+      linterForFs.Lint Constants.FakeFsPath badListSpaceFunAppTest
     ) |> ignore
 
   [<TestMethod>]

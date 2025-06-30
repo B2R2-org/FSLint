c876b57e5e043d4c405b36994c508d4a7ed56985
Jun Tae Kim <gnrkr789@kaist.ac.kr>
2025-06-30 10:09:18 +0900

    Add test for app indexer

--- src/FSLint.Tests/ArrayOrListTests.fs
+++ src/FSLint.Tests/ArrayOrListTests.fs
@@ -144,6 +144,22 @@ let bad = [
 [ this.Address +  uint64 this.Length + this.Name ]
 """
 
+  let goodListAppIndexerTest = """
+[ lifter.File.RawBytes[ptr.Offset] ]
+"""
+
+  let badListAppIndexerTest = """
+[ lifter.File.RawBytes[ ptr.Offset ] ]
+"""
+
+  let goodListAppIndexerInRangeTest = """
+good[1..]
+"""
+
+  let badListAppIndexerInRangeTest = """
+bad[ 1.. ]
+"""
+
   let goodListSpaceFunAppTest = """[ fn 1 2 3 x ]"""
 
   let badListSpaceFunAppTest = """[ fn  1 2 3 x ]"""
@@ -332,6 +348,20 @@ match bad with
     linterForFs.Lint Constants.FakeFsPath goodListSpaceInfixTest
     Assert.ThrowsException<LintException> (fun () ->
       linterForFs.Lint Constants.FakeFsPath badListSpaceInfixTest
+    ) |> ignore
+
+  [<TestMethod>]
+  member _.``[ArrayOrList] List App Indexer Bracket Spacing Test`` () =
+    linterForFs.Lint Constants.FakeFsPath goodListAppIndexerTest
+    Assert.ThrowsException<LintException> (fun () ->
+      linterForFs.Lint Constants.FakeFsPath badListAppIndexerTest
+    ) |> ignore
+
+  [<TestMethod>]
+  member _.``[ArrayOrList] List App Indexer Index Range Test`` () =
+    linterForFs.Lint Constants.FakeFsPath goodListAppIndexerInRangeTest
+    Assert.ThrowsException<LintException> (fun () ->
+      linterForFs.Lint Constants.FakeFsPath badListAppIndexerInRangeTest
     ) |> ignore
 
   [<TestMethod>]

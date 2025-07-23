de50892f880058556b3710031affeb76b30142d9
Jun Tae Kim <gnrkr789@kaist.ac.kr>
2025-07-23 16:48:08 +0900

    Fix Type Constructor can detect Default

--- src/FSLint/TypeConstructor.fs
+++ src/FSLint/TypeConstructor.fs
@@ -7,12 +7,21 @@ let checkConstructorSpacing src targetType = function
   | SynExpr.Const(SynConst.Unit, range)
   | SynExpr.Paren(range = range) ->
     match targetType with
-    | SynType.LongIdent(longDotId = SynLongIdent(id = id)) ->
-      match id with
-      | [ id ] when FunctionCallConvention.isPascalCase id.idText ->
+    (* Default constructor *)
+    | SynType.Var(typar = SynTypar(id, TyparStaticReq.None, false)) ->
+      if FunctionCallConvention.isPascalCase id.idText then
         if id.idRange.EndColumn <> range.StartColumn then
           reportError src range "Contains invalid whitespace"
         else ()
-      | _ -> ()
+      elif id.idRange.EndColumn + 1 <> range.StartColumn then
+        reportError src range "Contains invalid whitespace"
+      else ()
+    | SynType.Var _ as typ -> warn $"[Constructor]TODO: {typ}"
+    | SynType.LongIdent(longDotId = SynLongIdent(id = id)) ->
+      let id = List.last id
+      if FunctionCallConvention.isPascalCase id.idText
+        && id.idRange.EndColumn <> range.StartColumn
+      then reportError src range "Contains invalid whitespace"
+      else ()
     | _ -> ()
   | _ -> ()

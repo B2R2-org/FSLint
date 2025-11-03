9c7806441b8d925ced75778dbb6a209ac0d42a15
PSuHyeon <0111suhyeon@kaist.ac.kr>
2025-11-03 13:09:15 +0900

    Fix code style

--- src/FSLint/TryWithConvention.fs
+++ src/FSLint/TryWithConvention.fs
@@ -3,22 +3,18 @@ module B2R2.FSLint.TryWithConvention
 open FSharp.Compiler.Text
 open FSharp.Compiler.Syntax
 
-
 let checkSingleCaseBar (src: ISourceText) (clauses: SynMatchClause list) =
-  match clauses with
-  | [ singleClause ] ->
-    let SynMatchClause(trivia = trivia) = singleClause
-    match trivia.BarRange with
-    | Some barR ->
-      let barLine = src.GetLineString(barR.StartLine - 1)
-      let barText =
-        barLine.Substring(barR.StartColumn, barR.EndColumn - barR.StartColumn)
-      if barText.Trim() = "|" then
-        reportError src barR
-          "Remove unnecessary '|' for single exception case in try-with"
-    | None -> ()
-  | _ ->
-    ()
+  let SynMatchClause(trivia = trivia) = clauses.Head
+  match trivia.BarRange with
+  | Some barR ->
+    let barLine = src.GetLineString(barR.StartLine - 1)
+    let barText =
+      barLine.Substring(barR.StartColumn, barR.EndColumn - barR.StartColumn)
+    if barText.Trim() = "|" then
+      reportError src barR
+        "Remove unnecessary '|' for single exception case in try-with"
+    else ()
+  | None -> ()
 
 let check (src: ISourceText) (clauses: SynMatchClause list) =
   checkSingleCaseBar src clauses

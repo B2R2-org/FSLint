3f41c0de9f02a8223e1627aeb35cfcf1586e7440
Jun Tae Kim <gnrkr789@kaist.ac.kr>
2026-07-03 14:07:15 +0900

    Add missing expr to type annotation
      - Remove duplicate warns
      - Fix wrong msg

--- src/FSLint.Tests/Lint.fs
+++ src/FSLint.Tests/Lint.fs
@@ -3,6 +3,7 @@ module Lint
 
 open B2R2.FSLint
 open B2R2.FSLint.Program
+open FSharp.Compiler.Text
 open Microsoft.VisualStudio.TestTools.UnitTesting
 
 let lint context =
@@ -22,3 +23,13 @@ let lintAssertMsg (expected: string) context =
   match ex :> exn with
   | LintException msg -> StringAssert.Contains(msg, expected)
   | _ -> Assert.Fail "Expected LintException"
+
+let lintErrors (source: string) =
+  isStrict <- true
+  let context =
+    { Errors = []
+      Source = SourceText.ofString source
+      FilePath = FakeFsPath
+      EditorConfig = Configuration.defaultSettings }
+  linterForFsWithContext(Some context).Lint(FakeFsPath, source)
+  List.rev context.Errors
